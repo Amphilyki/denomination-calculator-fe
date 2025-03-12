31837add4175f83ec41668c25b466d21c016868f
Amphilyki <adonia_th@hotmail.com>
2025-03-12 14:11:43 +0100

    test: api services

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import apiService from './api.service';
 
 describe('ApiService', () => {
+  let httpController: HttpTestingController;
   let service: apiService;
+  let url = 'http://localhost:8080/euro-denomination-calculator/';
+  let response: Map<string, number> = new Map([['200.00', 1]]);
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -11,9 +17,33 @@ describe('ApiService', () => {
       providers: [apiService],
     });
     service = TestBed.inject(apiService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call getDenominations and return an map', () => {
+    service.getDenominations(1).subscribe((res) => {
+      expect(res).toEqual(response);
+    });
+    const req = httpController.expectOne({
+      method: 'GET',
+      url: `${url}1`,
+    });
+
+    req.flush(response);
+  });
+
+  it('should call getDenominationsDifference and return an map', () => {
+    service.getDenominationsDifference(1, 2).subscribe((res) => {
+      expect(res).toEqual(response);
+    });
+    const req = httpController.expectOne({
+      method: 'GET',
+      url: `${url}1/difference-from/2`,
+    });
+    req.flush(response);
   });
 });
