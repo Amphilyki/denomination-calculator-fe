7bbdd7e1362d8dd881eeed7b96a11519fa338716
Amphilyki <adonia_th@hotmail.com>
2025-03-12 13:49:26 +0100

    refactor: url as const

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,19 +6,16 @@ import { inject, Injectable } from '@angular/core';
 })
 export default class ApiService {
   http = inject(HttpClient);
+  backendUrl: string = 'http://localhost:8080/euro-denomination-calculator/';
 
   getDenominations(amount: number) {
-    const url = 'http://localhost:8080/euro-denomination-calculator/' + amount;
+    const url = this.backendUrl + amount;
     console.log('sending GET request to ' + url);
     return this.http.get<Map<string, number>>(url);
   }
 
   getDenominationsDifference(newAmount: number, oldAmount: number) {
-    const url =
-      'http://localhost:8080/euro-denomination-calculator/' +
-      newAmount +
-      '/difference-from/' +
-      oldAmount;
+    const url = this.backendUrl + newAmount + '/difference-from/' + oldAmount;
     console.log('sending GET request to ' + url);
     return this.http.get<Map<string, number>>(url);
   }
